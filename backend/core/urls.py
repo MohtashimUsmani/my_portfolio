589a1e10ebced4f78a28e96df93ff24aa70a9b98
M.Mohtashim Uddin Usmani <143964501+MohtashimUsmani@users.noreply.github.com>
2026-05-12 15:21:50 -0700

    Update urls.py

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -15,11 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, re_path
+from django.urls import path, re_path, include
 from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # This catches everything else and sends it to your Frontend
+    path('api/', include('api.urls')), # Your backend API
+    
+    # This catches everything else and sends it to your React App
     re_path(r'^.*$', TemplateView.as_view(template_name='index.html')),
 ]
